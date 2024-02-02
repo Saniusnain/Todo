60ef1ccd1ca6562f6ccde948e5eacb25d466cc28
sani.usnain <sani.usnain@onescreensolutions.com>
2024-02-02 16:11:54 +0500

    added required states

--- src/components/Todo/TodoComponent.tsx
+++ src/components/Todo/TodoComponent.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import {useState} from 'react';
 
-const TodoComponent = () => {
-  return (
-    <div>TodoComponent</div>
-  )
+
+interface Todo {
+	_id: string;
+	text: string;
+	_value: number;
+	completed: boolean;
 }
 
-export default TodoComponent
+const TodoComponent = () => {
+    const [listItems, setListItems] = useState<Todo[]>([]);
+	const [completedListItems, setcompletedListItems] = useState<Todo[]>([]);
+	const [listItem, setListItem] = useState<string>('');
+	const [edit, setEdit] = useState<boolean>(false);
+	const [todoId, setTodoId] = useState<Todo | undefined>();
+
+	return (
+		<div className='todo_container'>
+			<h1>ToDo List</h1>
+		</div>
+	);
+};
+
+export default TodoComponent;
