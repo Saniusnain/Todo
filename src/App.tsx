431c4b7a63e3dd163eaa75311ababb6e3a91ac4b
sani.usnain <sani.usnain@onescreensolutions.com>
2024-02-06 17:18:50 +0500

    added favicon and icon

--- src/App.tsx
+++ src/App.tsx
@@ -9,11 +9,11 @@ import Signup from './components/Authenticate/Signup';
 
 function App() {
 	return (
-		<>
+		<div className='bg-blue-800'>
 			{/* <TodoComponent />
 			<Login /> */}
 			<Signup />
-		</>
+		</div>
 	);
 }
 
--- src/components/Authenticate/Signup.tsx
+++ src/components/Authenticate/Signup.tsx
@@ -1,4 +1,5 @@
 import { useState, ChangeEvent } from 'react';
+import { TiWavesOutline } from 'react-icons/ti';
 
 const Signup = () => {
 	const [email, setEmail] = useState('');
@@ -6,9 +7,15 @@ const Signup = () => {
 	const [password, setPassword] = useState('');
 
 	return (
-		<div className='flex flex-col items-center justify-around h-screen font-mono '>
-			<h1 className='text-5xl font-bold text-center'>Task Flow</h1>
-			<div className='flex flex-col items-center justify-center w-2/4 py-10 space-y-3 bg-white rounded shadow-2xl ring-2 ring-offset-4 ring-amber-500'>
+		<div className='flex flex-col items-center justify-center h-screen font-mono '>
+			<h1 className='flex items-center justify-center mb-10 font-bold text-center text-white max-sm:flex-col sm:flex-row max-sm:text-3xl sm:text-5xl'>
+				Task Flow{' '}
+				<span className='ml-4 text-6xl'>
+					<TiWavesOutline />
+				</span>
+			</h1>
+
+			<div className='flex flex-col items-center justify-center py-10 space-y-3 bg-white rounded shadow-2xl max-sm:w-72 sm:w-2/4 sm:h-2/4 ring-2 ring-offset-4'>
 				<input
 					type='email'
 					placeholder='email'
@@ -16,7 +23,7 @@ const Signup = () => {
 					onChange={(e: ChangeEvent<HTMLInputElement>) =>
 						setEmail(e.target.value)
 					}
-					className='px-2 py-3 bg-transparent rounded outline-none ring-2 ring-slate-700 focus:'
+					className='w-2/4 px-2 py-3 bg-transparent rounded outline-none ring-2 ring-blue-800'
 					required
 				/>
 				<input
@@ -26,7 +33,7 @@ const Signup = () => {
 					onChange={(e: ChangeEvent<HTMLInputElement>) =>
 						setName(e.target.value)
 					}
-					className='px-2 py-3 bg-transparent rounded outline-none ring-2 ring-slate-700 focus:'
+					className='w-2/4 px-2 py-3 bg-transparent rounded outline-none ring-2 ring-blue-800'
 					required
 				/>
 				<input
@@ -36,10 +43,10 @@ const Signup = () => {
 					onChange={(e: ChangeEvent<HTMLInputElement>) =>
 						setPassword(e.target.value)
 					}
-					className='px-2 py-3 bg-transparent rounded outline-none ring-2 ring-slate-700 focus:'
+					className='w-2/4 px-2 py-3 bg-transparent rounded outline-none ring-2 ring-blue-800'
 					required
 				/>
-				<button className='px-4 py-2 my-4 text-xl font-semibold text-white rounded bg-amber-500 hover:bg-amber-600'>
+				<button className='px-4 py-2 my-4 text-xl font-semibold text-white transition bg-pink-300 rounded hover:translate-x-1 hover:translate-y-1 hover:bg-pink-400'>
 					Register
 				</button>
 			</div>
