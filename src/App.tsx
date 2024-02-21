5853c2e6bbd0e339fb3ceab798e31572fc828ef0
sani.usnain <sani.usnain@onescreensolutions.com>
2024-02-21 13:16:50 +0500

    404 page enabled

--- src/App.tsx
+++ src/App.tsx
@@ -37,7 +37,7 @@ function App() {
                 <Route element={<PrivateRoutes />}>
                   <Route path="/" element={<TodoComponent />} />
                 </Route>
-                {/* <Route path="*" element={<NotFound />} /> */}
+                <Route path="*" element={<NotFound />} />
               </Routes>
             </Suspense>
           </BrowserRouter>
