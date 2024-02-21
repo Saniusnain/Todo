35b30877092d08633c90576751b845d7463d9ee6
sani.usnain <sani.usnain@onescreensolutions.com>
2024-02-21 13:28:24 +0500

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    assetsDir: './',
-    rollupOptions: {
-      input: 'src/main.tsx', // adjust the entry file as per your project structure
-    },
-  },
-  server: {
-    proxy: {
-      '/': {
-        target: 'https://todo-server-393f0ac60da7.herokuapp.com',
-        changeOrigin: true,
-      },
-    },
-    fs: {
-      strict: false,
-    },
-  },
-});
+})
