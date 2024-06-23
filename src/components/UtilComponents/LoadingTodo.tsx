c623552b9c8420455fb23804a7e843a98d979822
Atom-cell <rajpootsaniusnain@gmail.com>
2024-06-23 13:14:04 +0500

    Animation pulse only for loading fixed

--- src/components/UtilComponents/LoadingTodo.tsx
+++ src/components/UtilComponents/LoadingTodo.tsx
@@ -1,18 +1,33 @@
-
-const LoadingTodo = ({text}:{text?:string}) => {
+const LoadingTodo = ({ text }: { text?: string }) => {
 	return (
 		<div className='flex items-center justify-between my-1 py-2 max-sm:px-5 sm:px-5'>
 			<div className='flex items-center'>
-				<div className='w-3 h-3 rounded bg-slate-400 animate-pulse '></div>
+				<div
+					className={`w-3 h-3 rounded bg-slate-400 ${
+						text?.includes('No') ? '' : 'animate-pulse'
+					}`}
+				></div>
 				<div className='flex flex-col'>
 					<div className='ml-5 flex items-center sm:text-lg font-semibold'>
-						{text ? <p className='text-slate-400 font-xs'>{text}</p>:<div className='h-1 w-48 bg-slate-400 animate-pulse'></div>}
+						{text ? (
+							<p className='text-slate-400 font-xs'>{text}</p>
+						) : (
+							<div className='h-1 w-48 bg-slate-400 animate-pulse'></div>
+						)}
 					</div>
 				</div>
 			</div>
 			<div className='flex ml-3'>
-				<div className='w-3 h-3 rounded bg-slate-400 animate-pulse mr-3'></div>
-				<div className='w-3 h-3 rounded bg-slate-400 animate-pulse'></div>
+				<div
+					className={`w-3 h-3 rounded bg-slate-400 ${
+						text?.includes('No') ? '' : 'animate-pulse'
+					} mr-3`}
+				></div>
+				<div
+					className={`w-3 h-3 rounded bg-slate-400 ${
+						text?.includes('No') ? '' : 'animate-pulse'
+					}`}
+				></div>
 			</div>
 		</div>
 	);
